5d332cc53340647abaeb0346dcaff07d7939733d
Jiwoo Lim <dlawldn10@gmail.com>
2022-08-05 23:18:46 +0900

    Algorithm study

--- Python_AlgorithmStudy/Python_AlgorithmStudy/취코테/#취코테 0727.py
+++ Python_AlgorithmStudy/Python_AlgorithmStudy/취코테/#취코테 0727.py
@@ -326,27 +326,56 @@
 # print(ans)
 
 
-#1449
-N, L = map(int, input().split())
-ary = list(map(int, input().split()))
-ary.sort()
-inter = []
-for i in range(len(ary)-1):
-    inter.append(ary[i+1]-ary[i])
+# #1449
+# N, L = map(int, input().split())
+# ary = list(map(int, input().split()))
+# ary.sort()
+# inter = []
+# for i in range(len(ary)-1):
+#     inter.append(ary[i+1]-ary[i])
 
-cnt = 0
+# cnt = 0
+# ans = 0
+
+# for v in inter:
+#     if cnt + v <= L-1:
+#         cnt += v
+#     else:
+#         cnt = 0
+#         ans += 1
+
+
+# print(ans+1)
+
+
+#11724
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+N, M = map(int, input().split())
+adj = [[False]*(N+1) for _ in range(N+1)]
+for _ in range(M):
+    u, v = map(int, input().split())
+    adj[u][v] = True
+    adj[v][u] = True
+
 ans = 0
+chk = [False]*(N+1)
 
-for v in inter:
-    if cnt + v <= L-1:
-        cnt += v
-    else:
-        cnt = 0
+def dfs(i):
+    for j in range(1, N+1):
+        if adj[i][j] == True and chk[j] == False:
+            chk[j] = True
+            dfs(j)
+
+for i in range(1, N+1):
+    if chk[i] == False:
         ans += 1
+        chk[i] = True
+        dfs(i)
 
 
-print(ans+1)
-
+print(ans)
 
 
     
