207600493a989dad027d7f9af47231ed88d3220a
Jiwoo Lim <dlawldn10@gmail.com>
2022-09-02 22:30:52 +0900

    Algorithm study

--- Python_AlgorithmStudy/Python_AlgorithmStudy/취코테/#취코테 0727.py
+++ Python_AlgorithmStudy/Python_AlgorithmStudy/취코테/#취코테 0727.py
@@ -996,46 +996,61 @@
 # print(ans)
 
 
-# 1389
-from collections import deque
+# # 1389
+# from collections import deque
 
-N, M = map(int, input().split())
-gr = [[False] * N for _ in range(N)]
-for _ in range(M):
-    a, b = map(int, input().split())
-    gr[a-1][b-1] = gr[b-1][a-1] = True
+# N, M = map(int, input().split())
+# gr = [[False] * N for _ in range(N)]
+# for _ in range(M):
+#     a, b = map(int, input().split())
+#     gr[a-1][b-1] = gr[b-1][a-1] = True
 
-ans = -1
-ans_tot = 987654321
-dist = [[0] * N for _ in range(N)]
+# ans = -1
+# ans_tot = 987654321
+# dist = [[0] * N for _ in range(N)]
 
-def bfs(start, goal):
-    chk = [False]*N
-    dq = deque()
-    chk[start] = True
-    dq.append((start, 0))
-    while dq:
-        now, d = dq.popleft()
-        if now == goal:
-            return d
+# def bfs(start, goal):
+#     chk = [False]*N
+#     dq = deque()
+#     chk[start] = True
+#     dq.append((start, 0))
+#     while dq:
+#         now, d = dq.popleft()
+#         if now == goal:
+#             return d
 
-        for nxt in range(N):
-            if gr[now][nxt] and not chk[nxt]:
-                chk[nxt] = True
-                dq.append((nxt, d+1))
+#         for nxt in range(N):
+#             if gr[now][nxt] and not chk[nxt]:
+#                 chk[nxt] = True
+#                 dq.append((nxt, d+1))
 
 
-for i in range(N):
-    tot = 0
-    for j in range(N):
-        if i != j:
-            if dist[i][j] == 0:
-                dist[i][j] = dist[j][i] = bfs(i, j)
+# for i in range(N):
+#     tot = 0
+#     for j in range(N):
+#         if i != j:
+#             if dist[i][j] == 0:
+#                 dist[i][j] = dist[j][i] = bfs(i, j)
 
-            tot += dist[i][j]
+#             tot += dist[i][j]
 
-    if ans_tot > tot:
-        ans = i
-        ans_tot = tot
+#     if ans_tot > tot:
+#         ans = i
+#         ans_tot = tot
 
-print(ans+1)
+# print(ans+1)
+
+
+# 1915
+n, m = map(int,input().split())
+arr = [ list(map(int,input())) for _ in range(n)]
+ans = max(arr[0])
+
+for i in range(1, n):
+    for j in range(1, m):
+        if arr[i][j] == 1:
+            arr[i][j] = min(arr[i-1][j-1], arr[i-1][j], arr[i][j-1]) + 1
+
+    ans = max(ans, max(arr[i]))
+
+print(ans**2)
