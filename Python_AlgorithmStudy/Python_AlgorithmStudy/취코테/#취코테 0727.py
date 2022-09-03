e54c0aaea1c668414115a14dc3e35989d0da4bf2
Jiwoo Lim <dlawldn10@gmail.com>
2022-09-03 19:32:54 +0900

    Algorithm study

--- Python_AlgorithmStudy/Python_AlgorithmStudy/취코테/#취코테 0727.py
+++ Python_AlgorithmStudy/Python_AlgorithmStudy/취코테/#취코테 0727.py
@@ -1041,16 +1041,37 @@
 # print(ans+1)
 
 
-# 1915
-n, m = map(int,input().split())
-arr = [ list(map(int,input())) for _ in range(n)]
-ans = max(arr[0])
+# # 1915
+# n, m = map(int,input().split())
+# arr = [ list(map(int,input())) for _ in range(n)]
+# ans = max(arr[0])
 
-for i in range(1, n):
-    for j in range(1, m):
-        if arr[i][j] == 1:
-            arr[i][j] = min(arr[i-1][j-1], arr[i-1][j], arr[i][j-1]) + 1
+# for i in range(1, n):
+#     for j in range(1, m):
+#         if arr[i][j] == 1:
+#             arr[i][j] = min(arr[i-1][j-1], arr[i-1][j], arr[i][j-1]) + 1
 
-    ans = max(ans, max(arr[i]))
+#     ans = max(ans, max(arr[i]))
 
-print(ans**2)
+# print(ans**2)
+
+
+# 2696
+# 다시 해보기
+for _ in range(int(input())):
+    M = int(input())
+    ipt = []
+    for _ in range(M//10+1 if M%10 else M//10):
+        ipt += map(int, input().split())
+
+    arr = []
+    ans = []
+    for i, v in enumerate(ipt):
+        arr.append(v)
+        if (i&1) == 0:
+            arr.sort()
+            ans.append(arr[i//2])
+        
+    print(len(ans))
+    for i in range(len(ans) // 10+1 if len(ans) % 10 else len(ans)//10):
+        print(*ans[i*10:(i+1)*10])
