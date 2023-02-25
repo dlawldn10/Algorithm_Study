5941dcfc818b5aa3206c12e01aab9f26a4adfef3
dlawldn10 <dlawldn10@gmail.com>
2023-02-25 16:40:34 +0900

    수정

--- Jungle/W13~.py
+++ Jungle/W13~.py
@@ -359,24 +359,29 @@
 
 # print(heapq.heappop(hq))
 
-# 정답 코드
-# mid를 행으로 나눈 몫을 더해서 해당 mid가 몇번째 숫자인지를 구하고, 그게 K번째 숫자가 맞으면 프린트한다.
-N, K = int(input()), int(input())
-start, end = 1, K
+# # 정답 코드
+# # mid를 행으로 나눈 몫을 더해서 해당 mid가 몇번째 숫자인지를 구하고, 그게 K번째 숫자가 맞으면 프린트한다.
+# N, K = int(input()), int(input())
+# start, end = 1, K
 
-while start <= end:
-    mid = (start + end) // 2
+# while start <= end:
+#     mid = (start + end) // 2
     
-    temp = 0
-    for i in range(1, N+1):
-        temp += min(mid//i, N) #mid 이하의 i의 배수 or 최대 N
+#     temp = 0
+#     for i in range(1, N+1):
+#         temp += min(mid//i, N) #mid 이하의 i의 배수 or 최대 N
     
-    if temp >= K: #이분탐색 실행
-        answer = mid
-        end = mid - 1
-    else:
-        start = mid + 1
-print(answer)
+#     if temp >= K: #이분탐색 실행
+#         answer = mid
+#         end = mid - 1
+#     else:
+#         start = mid + 1
+# print(answer)
+
+
+# -------------------------------------------------------------------------
+
+# 
 
 
 
